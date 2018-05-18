3f48ce9afa7bb508a1faa058ecd4bf5697073c03
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-18 18:43:49 +0300

    Move App to container

--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import 'typeface-roboto'
-import App from './App'
+import App from './containers/App/App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
