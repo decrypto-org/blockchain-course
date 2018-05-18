7d62759920a0c5036f0a0ce9e9e2fe8b5e7cbd96
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-18 18:55:11 +0300

    Change bootstrap

--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createBrowserHistory } from 'history'
+import { Router, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
 import 'typeface-roboto'
-import App from './containers/App/App'
+
+import indexRoutes from 'routes/index.js'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const loggerMiddleware = createLogger()
+const store = createStore(
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
+
+const hist = createBrowserHistory()
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Provider store={store}>
+      <Switch>
+        {indexRoutes.map((prop, key) => {
+          return <Route path={prop.path} component={prop.component} key={key} />
+        })}
+      </Switch>
+    </Provider>
+  </Router>,
+  document.getElementById('root')
+)
+
 registerServiceWorker()
