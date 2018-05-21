33706a6d5fbab51f2ceb4374b4a37238d9641efc
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-21 16:31:35 +0300

    Add routes

--- app/src/routes/app.js
+++ app/src/routes/app.js
@@ -1,3 +1,26 @@
-const appRoutes = []
+import LecturePage from 'views/Lecture/Lecture'
+import AssignmentPage from 'views/Assignment/Assignment'
+
+import {
+  Class,
+  Assignment
+} from '@material-ui/icons/'
+
+const appRoutes = [
+  {
+    path: '/lectures',
+    sidebarName: 'Lectures',
+    navbarName: 'Lectures',
+    icon: Class,
+    component: LecturePage
+  },
+  {
+    path: '/assignments',
+    sidebarName: 'Assignments',
+    navbarName: 'Assignments',
+    icon: Assignment,
+    component: AssignmentPage
+  }
+]
 
 export default appRoutes
