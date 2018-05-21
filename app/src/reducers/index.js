77b855ed99cd55bf90620ff9b9aad0a07c4107e2
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-21 16:31:18 +0300

    Create user reducer

---
+++ app/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux'
+import user from './user'
+
+const app = combineReducers({
+  user
+})
+
+export default app
---
+++ app/src/reducers/user.js
@@ -0,0 +1,10 @@
+const defaultState = {
+  id: 1,
+  email: ''
+}
+
+const user = (state = defaultState, action) => {
+  return state
+}
+
+export default user
