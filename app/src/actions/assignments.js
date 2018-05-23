d88da6416e2b705b20e176a7eb7fe88695d62257
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-24 02:44:10 +0300

    Create assignment actions

---
+++ app/src/actions/assignments.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+import {
+  GET_ASSIGNMENTS,
+  GET_ASSIGNMENTS_SUCCESS
+} from './actionTypes'
+
+const BASE_URL = process.env.API_URL || 'http://localhost:3000'
+
+const getAssignments = () => {
+  return {type: GET_ASSIGNMENTS, payload: {}}
+}
+
+const getAssignmentsSuccess = (data) => {
+  return {type: GET_ASSIGNMENTS_SUCCESS, payload: {data}}
+}
+
+const fetchAssignments = () => {
+  return async dispatch => {
+    dispatch(getAssignments())
+
+    const res = await axios.get(`${BASE_URL}/assignment`)
+
+    dispatch(getAssignmentsSuccess(res.data))
+  }
+}
+
+export {
+  fetchAssignments
+}
