a54cd1b50fc98e6e616d48eb747e56b435fc0a3c
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-24 02:52:43 +0300

    Combine lecture reducers

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import user from './user'
+import lectures from './lectures'
 import assignments from './assignments'
 
 const app = combineReducers({
   user,
+  lectures,
   assignments
 })
 
