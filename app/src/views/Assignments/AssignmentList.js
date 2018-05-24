9ae0d038c66da4da6f236a8dc6256bafbd13c38f
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-24 04:13:44 +0300

    List assignments

--- app/src/views/Assignments/AssignmentList.js
+++ app/src/views/Assignments/AssignmentList.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Grid from '@material-ui/core/Grid'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '../../components/List/ListItem'
 
 import {
   fetchAssignments
@@ -18,10 +21,16 @@ class AssignmentsList extends React.Component {
   }
 
   render () {
+    if (!this.props.assignments) {
+      return null
+    }
+
     return (
       <div>
         <Grid container>
-          Assignments...
+          <List component='nav' className='assignments'>
+            {this.props.assignments.map((item, index) => <ListItem key={index} {...item} to={'/assignment/' + item.id} />)}
+          </List>
         </Grid>
       </div>
     )
@@ -30,7 +39,7 @@ class AssignmentsList extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    lectures: state.lectures
+    assignments: state.assignments.assignments
   }
 }
 
