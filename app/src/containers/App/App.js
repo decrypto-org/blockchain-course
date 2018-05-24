22f31ac709398d2484a1e2bdfaf3acb25fdf9307
Christos Nasikas <xristosnasikas@gmail.com>
2018-05-25 00:58:05 +0300

    Add snackbar to app

--- app/src/containers/App/App.js
+++ app/src/containers/App/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { Switch, Route, Redirect } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Grid from '@material-ui/core/Grid'
 
 import '../../assets/css/App.css'
 import Header from '../../components/Header/Header.js'
 import Sidebar from '../../components/Sidebar/Sidebar.js'
+import Snackbar from '@material-ui/core/Snackbar'
+import IconButton from '@material-ui/core/IconButton'
+import Close from '@material-ui/icons/Close'
 import { isAuthenticated } from '../../utils/AuthService'
 
 import appRoutes from 'routes/app.js'
+
+import {
+  closeToast,
+  notify
+} from '../../actions'
 
 const switchRoutes = (
   <Switch>
@@ -20,6 +31,8 @@ const switchRoutes = (
     })}
   </Switch>
 )
+
+const styles = {}
 
 class App extends Component {
   render () {
@@ -33,9 +46,36 @@ class App extends Component {
             {switchRoutes}
           </main>
         </Grid>
+        <Snackbar
+          open={this.props.notification.open}
+          onClose={this.props.actions.closeToast}
+          message={this.props.notification.message}
+          action={[
+            <IconButton
+              key='close'
+              aria-label='Close'
+              color='inherit'
+              onClick={this.props.actions.closeToast}
+            >
+              <Close />
+            </IconButton>
+          ]}
+        />
       </div>
     )
   }
 }
 
-export default App
+const mapStateToProps = (state, ownProps) => {
+  return {
+    notification: state.notification
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    actions: bindActionCreators({closeToast, notify}, dispatch)
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(App))
