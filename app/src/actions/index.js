e7c4fe2fd38bb7889ba2a174f1d6c5fe34aaa296
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-08 15:59:40 +0300

    Create lecture actions

--- app/src/actions/index.js
+++ app/src/actions/index.js
@@ -1,4 +1,5 @@
 import lectureGroupsActions from './lectureGroups'
+import lectureActions from './lectures'
 import assignmentActions from './assignments'
 import { closeToast, notify } from './notifications'
 import { logout, unauthorize } from './user'
@@ -6,6 +7,7 @@ import { logout, unauthorize } from './user'
 export {
   lectureGroupsActions,
   assignmentActions,
+  lectureActions,
   closeToast,
   notify,
   logout,
