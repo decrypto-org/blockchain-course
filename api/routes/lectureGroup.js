984919bf48055b3abc5859be5612570d1fe6b807
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-08 15:36:37 +0300

    Refactor api routes

--- api/routes/lectureGroup.js
+++ api/routes/lectureGroup.js
@@ -1,37 +1,7 @@
-const express = require('express')
-const router = express.Router()
-const {LectureGroup, Lecture} = require('../models')
+const {createControllerRoutes} = require('../utils/routes')
+const LectureGroupController = require('../controllers/LectureGroupController')
 
-router.get(
-  '/',
-  async (req, res) => {
-    const groups = await LectureGroup.findAll()
-    return res.status(200).send(
-      {
-        success: true, groups
-      }
-    )
-  }
-)
-
-router.get(
-  '/:id(\\d+)',
-  async (req, res) => {
-    const group = await LectureGroup.findById(req.params.id, {include: [{
-      model: Lecture
-    }]})
-
-    if (group === null) {
-      return res.status(404).send({success: false, msg: 'Lecture group not found'})
-    }
-
-    const {Lectures: lectures, ...rest} = {...group.dataValues}
-    return res.status(200).send(
-      {
-        success: true, group: [{...rest, lectures}]
-      }
-    )
-  }
-)
+const controller = new LectureGroupController()
+const router = createControllerRoutes(controller)
 
 module.exports = router
