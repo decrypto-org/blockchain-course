ca29b49308727a2e7531037b721c0b711c867e57
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-09 20:05:59 +0300

    Refactor app routes

--- app/src/routes/app.js
+++ app/src/routes/app.js
@@ -1,3 +1,5 @@
+import {createAppRoutes} from '../utils/routes'
+
 import LectureGroupList from 'views/Lectures/LectureGroupList'
 import LectureGroup from 'views/Lectures/Group'
 import Lecture from 'views/Lecture/Lecture'
@@ -14,59 +16,37 @@ const appRoutes = [
   {
     path: '/lectureGroups',
     sidebarName: 'Lectures',
-    navbarName: 'Lectures',
     icon: Class,
     component: LectureGroupList,
     show: true
   },
   {
     path: '/lectureGroups/:id',
-    sidebarName: '',
-    navbarName: '',
-    icon: null,
-    component: LectureGroup,
-    show: false
+    component: LectureGroup
   },
   {
     path: '/lecture/:id',
-    sidebarName: '',
-    navbarName: '',
-    icon: null,
-    component: Lecture,
-    show: false
+    component: Lecture
   },
   {
     path: '/assignments',
     sidebarName: 'Assignments',
-    navbarName: 'Assignments',
     icon: Assignment,
     component: AssignmentList,
     show: true
   },
   {
     path: '/assignment/:id',
-    sidebarName: '',
-    navbarName: '',
-    icon: null,
-    component: SingleAssignment,
-    show: false
+    component: SingleAssignment
   },
   {
     path: '/login',
-    sidebarName: '',
-    navbarName: '',
-    icon: null,
-    component: () => { window.location = process.env.REACT_APP_LOGIN_URL },
-    show: false
+    component: () => { window.location = process.env.REACT_APP_LOGIN_URL }
   },
   {
     path: '/loginCallback',
-    sidebarName: '',
-    navbarName: '',
-    icon: null,
-    component: LoginCallback,
-    show: false
+    component: LoginCallback
   }
 ]
 
-export default appRoutes
+export default createAppRoutes(appRoutes)
