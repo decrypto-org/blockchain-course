d51192b4d415d28bd3e81bfd877cf466cdc6c692
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-10 18:56:45 +0300

    Return files with lecture

--- api/controllers/LectureController.js
+++ api/controllers/LectureController.js
@@ -1,8 +1,29 @@
 const BaseController = require('./BaseController')
-const {Lecture} = require('../models')
+const {Lecture, File} = require('../models')
 
 module.exports = class LectureController extends BaseController {
   constructor () {
     super(Lecture, 'lectures', 'lecture')
+  }
+
+  async read (req, res, id) {
+    const lecture = await Lecture.findById(id)
+
+    if (lecture === null) {
+      return res.status(404).send({success: false, msg: 'Lecture group not found'})
+    }
+
+    const files = await File.findAll({
+      where: {
+        type: 'lecture',
+        objId: id
+      }
+    })
+
+    return res.status(200).send(
+      {
+        success: true, lecture: [{...lecture.dataValues, files}]
+      }
+    )
   }
 }
