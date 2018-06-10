07f41ad5f8b0ce5afd523eab804f7338b747b02d
Christos Nasikas <xristosnasikas@gmail.com>
2018-06-10 18:56:53 +0300

    Show files

--- app/src/views/Lecture/LectureDetails.js
+++ app/src/views/Lecture/LectureDetails.js
@@ -4,7 +4,9 @@ import Item from '../../components/Item/Item'
 export default class LectureDetails extends React.Component {
   render () {
     const item = {name: 'lecture', title: this.props.item.title, description: this.props.item.description}
-    const contents = []
+    const contents = [
+      {title: 'Material', list: [...this.props.item.files], to: '/file'}
+    ]
     return (
       <Item item={item} contents={contents} />
     )
