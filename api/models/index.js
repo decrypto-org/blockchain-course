6c2bd9345a66670b31b8db1a576a940c86193bd2
Dimitris Lamprinos <pkakelas@gmail.com>
2018-08-09 18:16:11 +0300

    Add custom model import

--- api/models/index.js
+++ api/models/index.js
@@ -1,36 +1,39 @@
-'use strict';
+'use strict'
 
-var fs        = require('fs');
-var path      = require('path');
-var Sequelize = require('sequelize');
-var basename  = path.basename(__filename);
-var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+const CUSTOM_MODELS_PATH = __dirname + '/custom'
+
+const path = require('path')
+const Sequelize = require('sequelize')
+const basename = path.basename(__filename)
+const {mapJSFiles} = require('../utils/helpers.js')
+const env = process.env.NODE_ENV || 'development'
+const config = require(__dirname + '/../config/config.json')[env]
+const models = {}
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+mapJSFiles(__dirname, (file) => {
+  const model = sequelize['import'](path.join(__dirname, file))
+  models[model.name] = model
+}, [basename])
 
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+Object.keys(models).forEach(modelName => {
+  if (models[modelName].associate) {
+    models[modelName].associate(models)
   }
-});
+})
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+// Map custom models
+mapJSFiles(CUSTOM_MODELS_PATH, (file) => {
+  const model = require(path.join(CUSTOM_MODELS_PATH, file))
+  models[model.name] = model
+})
 
-module.exports = db;
+models.sequelize = sequelize
+models.Sequelize = Sequelize
+
+module.exports = models
--- api/utils/helpers.js
+++ api/utils/helpers.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+
 const isString = (str) => {
   return Object.prototype.toString.call(str) === '[object String]'
 }
@@ -6,7 +8,17 @@ const inPath = (arr, str) => {
   return arr.some(regexp => str.match(regexp))
 }
 
+const mapJSFiles = (dirname, mapFn, exclude = []) => {
+  fs
+    .readdirSync(dirname)
+    .filter(file => {
+      return (file.indexOf('.') !== 0) && !exclude.includes(file) && (file.slice(-3) === '.js')
+    })
+    .forEach(mapFn)
+}
+
 module.exports = {
   isString,
-  inPath
+  inPath,
+  mapJSFiles
 }
