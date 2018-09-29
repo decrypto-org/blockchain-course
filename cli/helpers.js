452a8519290524165bf855c24ce5b2509b8a1d9a
Christos Nasikas <xristosnasikas@gmail.com>
2018-09-29 21:59:36 +0300

    [CLI]: Create slugify helper function

---
+++ cli/helpers.js
@@ -0,0 +1,13 @@
+const slugify = (string) => {
+  return string
+    .toString()
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/--+/g, '-')
+    .replace(/^-+/, '')
+    .replace(/-+$/, '')
+}
+
+module.exports = {}
