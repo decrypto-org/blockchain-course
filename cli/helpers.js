288e24aa7f52929e60d8fb7907d52f397b61e51d
Christos Nasikas <xristosnasikas@gmail.com>
2018-09-29 22:06:00 +0300

    [CLI]: Create hash file helper function

--- cli/helpers.js
+++ cli/helpers.js
@@ -1,4 +1,6 @@
 const { sequelize } = require('blockchain-course-db').models
+const crypto = require('crypto')
+const fs = require('fs')
 
 const slugify = (string) => {
   return string
@@ -17,4 +19,17 @@ const printAndExit = (data, exitCode = 0) => {
   sequelize.close()
 }
 
-module.exports = {}
+const hashFile = (filePath) => {
+  return new Promise((resolve, reject) => {
+    const frs = fs.createReadStream(filePath)
+    const hash = crypto.createHash('sha256')
+
+    frs.on('error', reject)
+    frs.on('data', chunk => hash.update(chunk))
+    frs.on('end', () => resolve(hash.digest('hex')))
+  })
+}
+
+module.exports = {
+  hashFile
+}
