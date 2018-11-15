f29f21a523bba3b6a94bc52ccee3db6b40e495a9
Christos Nasikas <xristosnasikas@gmail.com>
2018-11-15 15:20:53 +0200

    [DB]: Fix relatioship between models

--- db/models/solution.js
+++ db/models/solution.js
@@ -2,11 +2,12 @@
 module.exports = (sequelize, DataTypes) => {
   var Solution = sequelize.define('Solution', {
     parameterizedAssignmentId: DataTypes.INTEGER,
+    studentId: DataTypes.INTEGER,
     data: DataTypes.TEXT
   }, {})
   Solution.associate = function (models) {
-    Solution.belongsTo(models.ParameterizedAssignment)
-    Solution.belongsTo(models.User, {as: 'student'})
+    Solution.belongsTo(models.ParameterizedAssignment, {foreignKey: 'parameterizedAssignmentId', targetKey: 'id'})
+    Solution.belongsTo(models.User, {as: 'student', foreignKey: 'studentId', targetKey: 'id'})
   }
   return Solution
 }
