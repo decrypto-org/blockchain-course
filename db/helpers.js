04c55b2bef7c3a39b03d2adb96fe35b71a0d3f08
Christos Nasikas <xristosnasikas@gmail.com>
2018-11-22 15:07:21 +0200

    [DB]: Add new helpers

--- db/helpers.js
+++ db/helpers.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto')
 const fs = require('fs')
+const should = require('chai').should()
 
 const isString = (str) => {
   return Object.prototype.toString.call(str) === '[object String]'
@@ -20,8 +21,50 @@ const sha256 = (data) => {
   return hash.digest('hex')
 }
 
+// https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/test/helpers/shouldFail.js
+
+const shouldFailWithMessage = async (promise, message) => {
+  try {
+    await promise
+  } catch (error) {
+    error.message.should.include(message, 'Wrong failure type')
+    return
+  }
+
+  should.fail(`Expected '${message}' failure not received`)
+}
+
+const shouldRevert = async (promise) => {
+  await shouldFailWithMessage(promise, 'revert')
+}
+
+const shouldThrow = async (promise) => {
+  await shouldFailWithMessage(promise, 'invalid opcode')
+}
+
+const shouldBeOutOfGas = async (promise) => {
+  await shouldFailWithMessage(promise, 'out of gas')
+}
+
+const expectEventInLogs = (events, eventName, eventArgs = {}) => {
+  const event = Object.keys(events).find((e) => {
+    if (e === eventName) {
+      for (const [k, v] of Object.entries(eventArgs)) {
+        events[e].returnValues[k].should.be.equal(v)
+      }
+      return true
+    }
+  })
+  should.exist(event)
+  return event
+}
+
 module.exports = {
   isString,
   mapJSFiles,
-  sha256
+  sha256,
+  shouldRevert,
+  shouldThrow,
+  shouldBeOutOfGas,
+  expectEventInLogs
 }
