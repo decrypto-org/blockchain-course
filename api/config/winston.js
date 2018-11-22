34e89bd854f08c2924350588eef0633f1c969e45
Christos Nasikas <xristosnasikas@gmail.com>
2018-11-22 15:08:02 +0200

    [API]: Colorize winston

--- api/config/winston.js
+++ api/config/winston.js
@@ -3,6 +3,7 @@ const { createLogger, format, transports } = require('winston')
 const logger = createLogger({
   level: 'info',
   format: format.combine(
+    format.colorize(),
     format.splat(),
     format.simple()
   ),
