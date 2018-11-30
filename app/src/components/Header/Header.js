fc625f9a51cfc99c472703fe1854ce6ba03a4c88
Christos Nasikas <xristosnasikas@gmail.com>
2018-12-01 01:03:57 +0200

    [APP]: Add toolbar and new avatar to header

--- app/src/components/Header/Header.js
+++ app/src/components/Header/Header.js
@@ -1,41 +1,21 @@
 import React from 'react'
 import cx from 'classnames'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
 
-const styles = {
-  appBar: {
-    zIndex: 10000
-  },
-  headerTitle: {
-    flex: 1
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20
-  }
-}
-
 function Header ({ ...props }) {
-  const { classes, Avatar } = props
+  const { Avatar, ToolBar } = props
   return (
-    <AppBar position='absolute' className={classes.appBar}>
-      <Toolbar>
-        <IconButton className={classes.menuButton} color='inherit' aria-label='Menu'>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant='title' color='inherit' className={cx(classes.headerTitle, 'header-link')}>
-          <Link to='/'>Blockchain Course</Link>
-        </Typography>
-        <Avatar />
-      </Toolbar>
-    </AppBar>
+    <header className='header container-fluid'>
+      <div className='row'>
+        <div className='col-6'>
+          <ToolBar />
+        </div>
+        <div className='col-6'>
+          <Avatar />
+        </div>
+      </div>
+    </header>
   )
 }
 
-export default withStyles(styles)(Header)
+export default Header
