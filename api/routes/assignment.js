7d763d7d90e586de481178ef24533948d102f3f9
Christos Nasikas <xristosnasikas@gmail.com>
2018-12-02 17:53:55 +0200

    [API]: Parametrized routes with path

--- api/routes/assignment.js
+++ api/routes/assignment.js
@@ -1,8 +1,6 @@
-const { createControllerRoutes } = require('../utils/routes')
-const AssignmentController = require('../controllers/AssignmentController')
+const { createSimpleRouter } = require('../utils/routes')
 
-const controller = new AssignmentController()
-const router = createControllerRoutes(controller)
+const { router, controller } = createSimpleRouter('AssignmentController', ':name', 'name')
 
 router.post(
   '/:name/solution',
@@ -10,13 +8,8 @@ router.post(
 )
 
 router.get(
-  '/:name',
-  (req, res, next) => { controller.read(req, res, req.params.name).catch(next) }
-)
-
-router.get(
   '/:name/material/:hash([0-9A-Fa-f]{64})',
-  (req, res) => { controller.download(req, res, req.params.id, req.params.hash) }
+  (req, res) => { controller.download(req, res, req.params.name, req.params.hash) }
 )
 
 module.exports = router
--- api/utils/routes.js
+++ api/utils/routes.js
@@ -1,17 +1,17 @@
 const express = require('express')
 
-const createControllerRoutes = (controller) => {
+const createControllerRoutes = (controller, path, param) => {
   const router = express.Router()
   router.get('/', (req, res, next) => controller.list(req, res).catch(next))
-  router.get('/:id(\\d+)', (req, res, next) => controller.read(req, res, req.params.id).catch(next))
+  router.get(`/${path}`, (req, res, next) => controller.read(req, res, req.params[param]).catch(next))
 
   return router
 }
 
-const createSimpleRouter = (key) => {
-  const Controller = require(`../controllers/${key}`)
+const createSimpleRouter = (controllerName, path = ':id(\\d+)', param = 'id') => {
+  const Controller = require(`../controllers/${controllerName}`)
   const controller = new Controller()
-  return { router: createControllerRoutes(controller), controller }
+  return { router: createControllerRoutes(controller, path, param), controller }
 }
 
 module.exports = {
