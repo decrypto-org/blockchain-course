dd2235cfb095eee29f0ca86449588348970059ca
Christos Nasikas <xristosnasikas@gmail.com>
2019-04-23 23:45:04 +0300

    Init websocket

--- app/src/index.js
+++ app/src/index.js
@@ -8,11 +8,14 @@ import indexRoutes from 'routes/index.js'
 import theme from './theme'
 import { setupInterceptors } from './utils/interceptors'
 import { unregister } from './registerServiceWorker'
+import { initWebsocket } from './actions/websocket'
 import Root from './containers/Root'
 
 const history = createBrowserHistory()
 const store = configureStore()
+
 setupInterceptors(store)
+initWebsocket(store)
 
 ReactDOM.render(
   <Root history={history} store={store} routes={indexRoutes} theme={theme} />,
